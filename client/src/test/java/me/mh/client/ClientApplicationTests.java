ff19a654c947c921925807f39116a94ef9ebbc81
mehdi <email2mehdihasan@gmail.com>
2019-11-06 11:55:56 +0100

    client project added

---
+++ client/src/test/java/me/mh/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mh.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
