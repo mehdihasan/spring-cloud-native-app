cae432f9302516cc8120ef904c3300a0fc594798
mehdi <email2mehdihasan@gmail.com>
2019-11-06 11:00:03 +0100

    Discovery server implemented

--- discovery-server/src/main/java/me/mh/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/me/mh/discoveryserver/DiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package me.mh.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // 1 - defining the Eureka server 
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
