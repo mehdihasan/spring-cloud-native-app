309a9a90681e9693419951b621a649598278dd6a
mehdi <email2mehdihasan@gmail.com>
2019-11-07 09:57:01 +0100

    implemented @Refreshscope

--- config-client-app/src/main/java/me/mh/configclientapp/ConfigClientAppApplication.java
+++ config-client-app/src/main/java/me/mh/configclientapp/ConfigClientAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient // 28 - enabling as a discovery client 
 @RestController // 34
+@RefreshScope // 41 - basically, we use @Refreshscope to update @Value and @Bean properties in RUNTIME
 public class ConfigClientAppApplication {
 
 	// 32
