7c0d41478be62cbfb9fb83dc85f963e54a72b62e
mehdi <email2mehdihasan@gmail.com>
2019-11-08 13:18:40 +0100

    Turbine app added

---
+++ turbine-app/src/main/java/me/mh/turbineapp/TurbineAppApplication.java
@@ -0,0 +1,13 @@
+package me.mh.turbineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineAppApplication.class, args);
+	}
+
+}
