3e59341989210518f32a7df703dbfc7543d5bef6
mehdi <email2mehdihasan@gmail.com>
2019-11-08 09:55:43 +0100

    ribbon time service and app created

---
+++ ribbon-time-app/src/main/java/me/mh/ribbontimeapp/RibbonTimeAppApplication.java
@@ -0,0 +1,13 @@
+package me.mh.ribbontimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeAppApplication.class, args);
+	}
+
+}
---
+++ ribbon-time-service/src/main/java/me/mh/ribbontimeservice/RibbonTimeServiceApplication.java
@@ -0,0 +1,13 @@
+package me.mh.ribbontimeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonTimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeServiceApplication.class, args);
+	}
+
+}
