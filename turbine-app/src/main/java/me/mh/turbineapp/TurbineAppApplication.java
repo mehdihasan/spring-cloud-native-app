7f9810c56df0e68edbae0500cf79398ca951c633
mehdi <email2mehdihasan@gmail.com>
2019-11-08 13:30:07 +0100

    enabled turbine but not working at all

--- turbine-app/src/main/java/me/mh/turbineapp/TurbineAppApplication.java
+++ turbine-app/src/main/java/me/mh/turbineapp/TurbineAppApplication.java
@@ -2,8 +2,10 @@ package me.mh.turbineapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine // 110
 public class TurbineAppApplication {
 
 	public static void main(String[] args) {
