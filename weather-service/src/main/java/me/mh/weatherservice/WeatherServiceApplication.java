a7235eb7ad4c6e48ad6cd15bb219c45512a923c5
mehdi <email2mehdihasan@gmail.com>
2019-11-08 12:05:09 +0100

    weather service is up and running

--- weather-service/src/main/java/me/mh/weatherservice/WeatherServiceApplication.java
+++ weather-service/src/main/java/me/mh/weatherservice/WeatherServiceApplication.java
@@ -1,13 +1,32 @@
 package me.mh.weatherservice;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController // 89
+@EnableDiscoveryClient // 90
 @SpringBootApplication
 public class WeatherServiceApplication {
 
+	
+	// 91
+	private String[] weather = new String[]{"Sunny", "Cloudy", "Rainy", "Windy"};
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherServiceApplication.class, args);
 	}
 
+	
+	// 92
+	@GetMapping("/weather")
+	public String getWeather() {
+		int rand = ThreadLocalRandom.current().nextInt(0, 4);
+		return weather[rand];
+	}
 }
